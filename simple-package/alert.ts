a4cdcddd13adf5ac491e9f062d2910fcc6fa0dfe
GongT <admin@gongt.me>
2017-09-27 19:58:10 +0800

    add alert function

---
+++ simple-package/alert.ts
@@ -0,0 +1,18 @@
+/* this file is just for debug */
+
+let IS_SERVER = typeof window === 'undefined';
+try {
+	const val = process.env.XXX;
+	require.resolve('fs');
+} catch (e) {
+	IS_SERVER = false;
+}
+const colorHolder = IS_SERVER ? '%s' : '%c';
+const errorColor = IS_SERVER ? '\x1B[38;5;9m' : 'color:red;font-size:large;';
+const resetColor = IS_SERVER ? '\x1B[0m' : 'color:black;font-size:normal;';
+
+export function alertJenv<T>(option: T, valueName: keyof T) {
+	if (option.hasOwnProperty(valueName)) {
+		console.error(`${colorHolder}参数%s应使用JsonEnv传入${colorHolder}`, errorColor, valueName, resetColor);
+	}
+}
